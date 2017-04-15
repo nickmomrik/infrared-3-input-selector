a78d917ebec1950fd7705ab155ae9a85272773d8
Nick Momrik <nick@momrik.com>
2017-04-15 13:22:28 -0400

    Add IR receiving.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-import RPi.GPIO as GPIO
 from time import sleep
+import RPi.GPIO as GPIO
+import pylirc
 
 # Pins
 pin_button_power = 18
@@ -33,6 +34,8 @@ GPIO.setup( pin_button_1, GPIO.IN )
 GPIO.setup( pin_button_2, GPIO.IN )
 GPIO.setup( pin_button_3, GPIO.IN )
 
+pylirc.init( 'pylirc', './pylirc.conf', 0 )
+
 def switch_device( led ) :
 	global active_led
 
@@ -54,16 +57,22 @@ def toggle_power() :
 
 try :
 	while ( True ) :
-		if ( GPIO.LOW == GPIO.input( pin_button_power ) ) :
+		s = pylirc.nextcode( 1 )
+		command = None
+		if ( s ) :
+			for ( code ) in s :
+				command = code["config"]
+
+		if ( 'KEY_POWER' == command or GPIO.LOW == GPIO.input( pin_button_power ) ) :
 			toggle_power()
 			while ( GPIO.LOW == GPIO.input( pin_button_power ) ) :
 				sleep( 0.1 )
 		elif ( power_on ) :
-			if ( GPIO.LOW == GPIO.input( pin_button_1 ) and pin_led_1 != active_led ) :
+			if ( pin_led_1 != active_led and ( 'KEY_1' == command or GPIO.LOW == GPIO.input( pin_button_1 ) ) ) :
 				switch_device( pin_led_1 )
-			elif ( GPIO.LOW == GPIO.input( pin_button_2 ) and pin_led_2 != active_led ) :
+			elif ( pin_led_2 != active_led and ( 'KEY_2' == command or GPIO.LOW == GPIO.input( pin_button_2 ) ) ) :
 				switch_device( pin_led_2 )
-			elif ( GPIO.LOW == GPIO.input( pin_button_3 ) and pin_led_3 != active_led ) :
+			elif ( pin_led_3 != active_led and ( 'KEY_3' == command or GPIO.LOW == GPIO.input( pin_button_3 ) ) ) :
 				switch_device( pin_led_3 )
 except KeyboardInterrupt :
 	print 'Exiting via CTRL+C...'
